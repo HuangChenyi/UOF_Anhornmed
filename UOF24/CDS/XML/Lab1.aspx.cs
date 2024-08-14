39d57bee339e8fb57de7dc71dc88bb632327cc34
woodman <woodman@edetw.com>
2024-08-14 10:50:33 +0800

    Lab範例

---
+++ UOF24/CDS/XML/Lab1.aspx.cs
@@ -0,0 +1,44 @@
+﻿using Ede.Uof.Utility.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+public partial class CDS_XML_Lab1 : BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        XElement xe = new XElement("ContactList");
+
+        XElement item01 = new XElement("Contact",
+                       new XAttribute("name", "Emma"),
+                                  new XAttribute("Phone", "0912345678"));
+        XElement item02 = new XElement("Contact",
+                       new XAttribute("name", "John"),
+                    new XAttribute("Phone", "0923456789"));
+        XElement item03 = new XElement("Contact",
+            new XAttribute("name", "Mary"),
+                    new XAttribute("Phone", "0934567890"));
+             
+        xe.Add(item01, item02, item03);
+        txtXML.Text = xe.ToString();
+    }
+
+    protected void btnGetValue_Click(object sender, EventArgs e)
+    {
+        XElement xe = XElement.Parse(txtXML.Text);  
+        var result = xe.Elements("Contact").Where(x => x.Attribute("name").Value == txtID.Text).FirstOrDefault();
+
+        if (result != null)
+        {
+            txtValue.Text = result.Attribute("Phone").Value;
+        }
+        else
+        {
+            txtValue.Text = "查無資料";
+        }
+    }
+}
---
+++ UOF24/CDS/XML/Lab2.aspx.cs
@@ -0,0 +1,61 @@
+﻿using Ede.Uof.Utility.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+public partial class CDS_XML_Lab2 : BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        XElement formXe = new XElement("Form"
+            , new XAttribute("formVersionId",Guid.NewGuid().ToString())
+            , new XAttribute("urgentLevel", "2"));
+
+        XElement applicantXe = new XElement("Applicant"
+                       , new XAttribute("account", "Tony")
+                                  , new XAttribute("groupId", "")
+                                  , new XAttribute("jobTitleId", ""));
+        XElement commentXe = new XElement("Comment");
+        commentXe.Value = "有事";
+
+        XElement formFieldValueXe = new XElement("FormFieldValue");
+        XElement noXe = new XElement("FieldItem",
+            new XAttribute("fieldId","NO"),
+            new XAttribute("fieldValue", "A001"));
+
+        XElement a01Xe = new XElement("FieldItem",
+           new XAttribute("fieldId", "A01"),
+           new XAttribute("fieldValue", "事假"));
+
+        XElement a02Xe = new XElement("FieldItem",
+           new XAttribute("fieldId", "A02"),
+           new XAttribute("fieldValue", "2024/08/14"));
+
+        formXe.Add(applicantXe,formFieldValueXe);
+        applicantXe.Add(commentXe);
+
+        formFieldValueXe.Add(noXe, a01Xe, a02Xe);
+
+        txtXML.Text = formXe.ToString();
+
+    }
+
+    protected void btnGetValue_Click(object sender, EventArgs e)
+    {
+        XElement xe = XElement.Parse(txtXML.Text);
+        var result = xe.Element("FormFieldValue").Elements("FieldItem").Where(x => x.Attribute("fieldId").Value == txtID.Text).FirstOrDefault();
+
+        if (result != null)
+        {
+            txtValue.Text = result.Attribute("fieldValue").Value;
+        }
+        else
+        {
+            txtValue.Text = "查無資料";
+        }
+    }
+}
